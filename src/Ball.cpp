d6478fe6bd3d7c2a7556d33707f47a9173920f3f
ligas10105 <ligass22@yahoo.com>
2023-06-06 13:44:41 +0200

    Ball and edges collisions add

---
+++ src/Ball.cpp
@@ -0,0 +1,71 @@
+//
+// Created by ligas on 06.06.2023.
+//
+
+#include "Ball.h"
+Ball::Ball(float cx, float cy) {
+    shape.setPosition(cx, cy);
+    shape.setRadius(radius);
+    shape.setFillColor(sf::Color::White);
+    shape.setOrigin(radius, radius);
+}
+
+    void Ball::draw(sf::RenderTarget &target, sf::RenderStates state) const {
+        target.draw(shape,state);
+    }
+
+    void Ball ::update() {
+    shape.move(velocity);
+    if (left() < 0){
+        velocity.x = ballVelocity;
+    }
+    else if (right() > 800){
+        velocity.x = -ballVelocity;
+    }
+    else if (top() < 0){
+        velocity.y = ballVelocity;
+    }
+    else if (bottom() > 600){
+        velocity.y = -ballVelocity;
+    }
+
+}
+float Ball :: left(){
+    return this -> shape.getPosition().x - shape.getRadius();
+}
+
+float Ball :: right(){
+    return this -> shape.getPosition().x + shape.getRadius();
+}
+
+float Ball :: top(){
+    return this -> shape.getPosition().y - shape.getRadius();
+}
+
+float Ball :: bottom(){
+    return this -> shape.getPosition().y + shape.getRadius();
+}
+
+void Ball :: moveDown(){
+    this -> velocity.y = -ballVelocity;
+}
+
+void Ball :: moveUp() {
+    this -> velocity.y = ballVelocity;
+}
+
+void Ball ::moveRight() {
+    this -> velocity.x = ballVelocity;
+}
+
+void Ball ::moveLeft() {
+    this -> velocity.x = -ballVelocity;
+}
+
+sf::Vector2f Ball ::getPosition() {
+    return shape.getPosition();
+}
+
+
+
+
---
+++ src/Ball.h
@@ -0,0 +1,44 @@
+//
+// Created by ligas on 06.06.2023.
+//
+
+#ifndef ARKANOID_BALL_H
+#define ARKANOID_BALL_H
+
+#include <SFML/Graphics.hpp>
+
+
+class Ball : public sf::Drawable {
+public:
+    Ball(float cx, float cy);
+
+    void update();
+
+    void moveUp();
+
+    void moveDown();
+
+    void moveRight();
+
+    void moveLeft();
+
+    sf::Vector2f getPosition();
+
+    float left();
+
+    float right();
+
+    float top();
+
+    float bottom();
+
+private:
+    sf::CircleShape shape;
+    const float radius{30.0};
+    const float ballVelocity{7.0};
+    sf::Vector2f velocity{ballVelocity, ballVelocity};
+    void draw(sf::RenderTarget& target, sf::RenderStates state) const override;
+};
+
+
+#endif //ARKANOID_BALL_H
